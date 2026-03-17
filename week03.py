115d8ffc8f2275a8598599c98769679d1645783e
Gyuwon Moon <gyuwon822@gmail.com>
2026-03-17 17:54:56 +0900

    pandas - create dataframe i

--- week03.py
+++ week03.py
@@ -1,9 +1,20 @@
 import numpy as np
 import pandas as pd
 
-items = {"a":[100,80,90],
-         "b":[95,75,85],
-         "c":[70,100,99]}
+#      a   b    c
+# 1  100  95   70
+# 2   80  75  100
+# 3   90  85   99
 
-df_items = pd.DataFrame(items, index=[1,2,3])
+# items = {"a":[100,80,90],
+#          "b":[95,75,85],
+#          "c":[70,100,99]}
+
+items = [
+    [100,80,90],
+    [95,75,85],
+    [70,100,99]
+]
+
+df_items = pd.DataFrame(items, index=[1,2,3], columns=["a","b","c"])
 print(df_items)
