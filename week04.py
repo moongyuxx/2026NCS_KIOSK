e80288d609150d77c784304af920ba2a94d6467c
Gyuwon Moon <gyuwon822@gmail.com>
2026-03-24 17:14:51 +0900

    pandas - groupby

--- week04.py
+++ week04.py
@@ -1,15 +1,10 @@
 import ssl
 import seaborn as sns
+import pandas as pd
 
 # SSL 인증 비활성화
 ssl._create_default_https_context = ssl._create_unverified_context
 
 mpg = sns.load_dataset('mpg')
-
-print(
-    mpg[['origin', 'name', 'mpg']]
-    .query('origin == "europe" and mpg >= 30.0')
-    .sort_values('mpg', ascending=False)
-)
-
-print(mpg)
+group_mpg = pd.DataFrame(mpg.groupby('origin'))
+print(group_mpg)
