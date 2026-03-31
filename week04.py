4e2a881d6c0eb03abe4fd41b0c190e0bc2602d00
Gyuwon Moon <gyuwon822@gmail.com>
2026-03-31 16:46:39 +0900

    pandas - groupby i

--- week04.py
+++ week04.py
@@ -3,8 +3,11 @@ import seaborn as sns
 import pandas as pd
 
 # SSL 인증 비활성화
-ssl._create_default_https_context = ssl._create_unverified_context
+# ssl._create_default_https_context = ssl._create_unverified_context
 
 mpg = sns.load_dataset('mpg')
-group_mpg = pd.DataFrame(mpg.groupby('origin'))
-print(group_mpg)
+print(mpg.info())
+hp_median = mpg['horsepower'].median()
+mpg_filed = mpg.copy()
+mpg_filled['horsepower'] = mpg_filled['horsepower'].fillna(hp_median)
+print(mpg_filled.info())
